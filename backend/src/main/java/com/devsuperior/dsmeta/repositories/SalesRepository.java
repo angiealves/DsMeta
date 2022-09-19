1433f89e6adb64dfedbcd3992bc39535550e0bc2
Angelo Alves <angelodaniel@hotmail.com>
2022-09-18 22:04:34 -0300

    Seleção de Data

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SalesRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SalesRepository.java
@@ -1,6 +1,11 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sales;
 
@@ -10,5 +15,8 @@ public interface SalesRepository extends JpaRepository<Sales, Long> {
 	 * nossa aplicação - salvar uma venda, buscar uma venda por ID, consultar, etc.
 	 * Através do extends que eu faço isso.
 	 */
+	
+	@Query("SELECT obj FROM Sales obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sales> findSales(LocalDate min, LocalDate max, Pageable pageable);
 	
 }
