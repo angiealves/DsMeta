e92b601df7b7bc9974dac0df8645239777337afe
Angelo Alves <angelodaniel@hotmail.com>
2022-09-19 00:10:06 -0300

    Correção variável de sms - Heroku

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsmeta.services;
 
+import java.text.DecimalFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -34,9 +36,13 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date 
-				+ " com um total de vendas no valor de R$ " + String.format("%.2f", sale.getAmount());
+//		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date 
+//				+ " com um total de vendas no valor de R$ " 
+//				+ new DecimalFormat("#,##0.00").format(sale.getAmount());
 
+		String msg = String.format("O vendedor %s foi destaque em %s com um total de vendas no valor de R$ %.2f. Parabéns!", 
+				sale.getSellerName(), date, sale.getAmount());
+		
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
